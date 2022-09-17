3f06eca20421549ce47a7a20031de38acfe37baf
unknown <mcbrooks@pm.me>
2022-09-17 15:51:32 -0400

    added comments for training and visual testing

--- rl-model.py
+++ rl-model.py
@@ -1,15 +1,17 @@
+# import gym library and PPO model
 import gym
-
-env = gym.make("LunarLander-v2")
 from stable_baselines3 import PPO
 
+# create & initialize environment 
+env = gym.make("LunarLander-v2")
 env.reset()
 
+# load PPO model and train for 100k time steps
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=100000)
 
+# after training, the agent will use the trained model for 10 episodes and render to screen
 episodes = 10
-
 for ep in range(episodes):
     obs = env.reset()
     done = False
@@ -17,4 +19,5 @@ for ep in range(episodes):
         env.render()
         obs, reward, done, info = env.step(env.action_space.sample())
 
+#close the environment
 env.close()
